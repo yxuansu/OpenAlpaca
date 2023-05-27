2f0927451d3b0dd39ace393c43b01d2e3db231bb
Yixuan Su <syxistheone@gmail.com>
2023-05-27 15:56:22 +0100

    Add files via upload

---
+++ scripts/train_openalpaca_7b.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+deepspeed --include localhost:0,1,2,3,4,5,6,7 --master_addr 127.0.0.1 --master_port 28457 train_sft.py \
+    --model openllama \
+    --model_path  openllmplayground/openalpaca_7b_700bt_preview\
+    --data_path ./openalpaca.json \
+    --max_length 1024\
+    --save_path  ./ckpt/openalpaca_7b/7bt_preview/\
+    --log_path ./ckpt/openalpaca_7b/7bt_preview/rest
+
+
